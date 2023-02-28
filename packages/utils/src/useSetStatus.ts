8d86f3235520bad26b83fc5d147ee4983d83a2fc
谢伟良 <642997741@qq.com>
2023-02-28 11:26:29 +0800

    feat: Update package name

--- packages/utils/src/useSetStatus.ts
+++ packages/utils/src/useSetStatus.ts
@@ -1,21 +1,20 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react'
 
-export default function useSetStatus<T extends { getStatus: () => any; setStatus: (opt: any) => void }, F = {}>(
-    instance: T,
-    props = {} as F,
-    propsName: string[] = [],
-  ) {
-    propsName.forEach((name) => {
-      const eName = name as keyof F;
-      const [state, setState] = useState(props[eName]);
-      useEffect(() => {
-        if (instance && props[eName] !== undefined) {
-          if (props[eName] !== state) {
-            const status = instance.getStatus();
-            instance.setStatus({ ...status, [eName]: props[eName] });
-            setState(props[eName]);
-          }
-        }
-      }, [instance, props[eName]]);
-    });
-  }
+export default function useSetStatus<
+	T extends { getStatus: () => any; setStatus: (opt: any) => void },
+	F = {}
+>(instance: T, props = {} as F, propsName: string[] = []) {
+	propsName.forEach((name) => {
+		const eName = name as keyof F
+		const [state, setState] = useState(props[eName])
+		useEffect(() => {
+			if (instance && props[eName] !== undefined) {
+				if (props[eName] !== state) {
+					const status = instance.getStatus()
+					instance.setStatus({ ...status, [eName]: props[eName] })
+					setState(props[eName])
+				}
+			}
+		}, [instance, props[eName]])
+	})
+}
